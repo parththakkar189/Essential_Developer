4addb76195a573d17ee80a623fd47b4d799a4557
Parth Takkhar <parththakkar189@gmail.com>
2023-12-02 21:48:20 -0500

    Move remote feed loader components to production.

--- EssentialFeedTests/FeedLoaderTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/FeedLoaderTests/RemoteFeedLoaderTests.swift
@@ -6,22 +6,7 @@
 //
 
 import XCTest
-
-class RemoteFeedLoader {
-    let client: HTTPClient
-    let url: URL
-    init(url: URL = URL(string: "https://a-url.com")!, client: HTTPClient) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import EssentialFeed
 
 final class RemoteFeedLoaderTests: XCTestCase {
     
