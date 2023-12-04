3689a4f7b6f249a1e9b1f07a3e2dd53b7f930771
Parth Takkhar <parththakkar189@gmail.com>
2023-12-03 22:42:53 -0500

    Deliver items array on 200 HTTP response with JSON items

--- EssentialFeed/Feed Feature/FeedItem.swift
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -6,10 +6,28 @@
 //
 
 import Foundation
-
+struct Root: Decodable {
+    let items: [FeedItem]
+}
 public struct FeedItem: Equatable {
-    let id: UUID
-    let description: String?
-    let locaiton: String
-    let imageURL: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, locaiton: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = locaiton
+        self.imageURL = imageURL
+    }
+}
+
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }
