167452ba640a5aaa5132ec0f5f670758758618b7
Parth Takkhar <parththakkar189@gmail.com>
2023-12-24 18:25:22 -0500

    guarantee we do not deliver a result (invoke the completion closure) after the remotefeedloader instance has been delocated.

--- EssentialFeedTests/FeedLoaderTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/FeedLoaderTests/RemoteFeedLoaderTests.swift
@@ -90,6 +90,19 @@ final class RemoteFeedLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_doesNotDeliverResultAfterSUIInstanceHasBeenDeallocated() {
+        let url = URL(string: "https://any-url.com")!
+        let client = HTTPClientSpy()
+        var sut: RemoteFeedLoader? = RemoteFeedLoader(url: url, client: client)
+        
+        var capturedResults = [RemoteFeedLoader.Result]()
+        sut?.load { capturedResults.append($0) }
+        
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+    
     //MARK: Helpers
     
     private func makeItem(id: UUID, description: String? = nil, location: String? = nil, imageURL: URL) -> (model: FeedItem, json: [String: Any]) {
