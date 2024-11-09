6b1ab8da7b69362fe93e1b30123bbb9d7d0ee337
Parth Takkhar <parththakkar189@gmail.com>
2024-11-09 17:27:49 -0500

    Added github CI actions.

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,16 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Parth Thakkar on 2024-11-09.
+//
+
+import Testing
+
+struct EssentialFeedAPIEndToEndTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
