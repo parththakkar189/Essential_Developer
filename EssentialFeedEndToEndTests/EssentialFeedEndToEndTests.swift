6ae26bdc54028066c8614887cffb4895a2754116
Parth Takkhar <parththakkar189@gmail.com>
2024-11-11 12:52:26 -0500

    Added test scheme for end to end tests

---
+++ EssentialFeedEndToEndTests/EssentialFeedEndToEndTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedEndToEndTests.swift
+//  EssentialFeedEndToEndTests
+//
+//  Created by Parth Thakkar on 2024-11-11.
+//
+
+import XCTest
+import EssentialFeed
+
+final class EssentialFeedEndToEndTests: XCTestCase {
+
+}
