8dd1795107f5265029862c63be0b5affff2098e2
Parth Takkhar <parththakkar189@gmail.com>
2024-12-08 14:44:02 -0500

    extract system under test creation to a factory method to prevent against breaking changes

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -31,21 +31,25 @@ class FeedStore {
 final class CacheFeedUseCaseTests: XCTestCase {
     
     func test_init_doesNotDeleteCachedUponCreation() {
-        let store = FeedStore()
-        _ = LocalFeedLoader(store: store)
+        let (_, store) = makeSUT()
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
     
     func test_save_requestsCacheDeletion() {
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store: store)
         let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
         sut.save(items)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
     }
     
     
     // MARK: - Helpers
+    
+    private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        return (sut, store)
+    }
     
     private func uniqueItem() -> FeedItem {
         FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
