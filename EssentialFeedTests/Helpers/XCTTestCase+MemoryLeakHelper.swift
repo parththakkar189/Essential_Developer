9499ece938211debffd4c6b522e69e6c6a9b1f2d
Parth Takkhar <parththakkar189@gmail.com>
2025-07-26 17:49:09 -0400

    Extract system under test (sut) to a factory method

--- EssentialFeedTests/Helpers/XCTTestCase+MemoryLeakHelper.swift
+++ EssentialFeedTests/Helpers/XCTTestCase+MemoryLeakHelper.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-extension XCTestCase {
+public extension XCTestCase {
     func trackForMemoryLeaks(_ instance: AnyObject,
                                      file: StaticString = #filePath,
                                      line: UInt = #line) {
