bf816e95ec6b817c41024f16ceb86966fc63d949
Parth Takkhar <parththakkar189@gmail.com>
2025-08-17 15:45:07 -0400

    Decouple `FeedImageViewModel` from `UIKit` by creating a transformation closure that converts an image `Data` value into a generic `Image` type.
    When composing with a UIKit user interface, we inject a closure to transform the image `Data` into `UIImage`

--- EssentialFeediOS/FeedUI/Controllers/FeedImageCellController.swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedImageCellController.swift
@@ -5,51 +5,49 @@
 //  Created by Parth Thakkar on 2025-08-16.
 //
 
-import EssentialFeed
 import UIKit
 
 final class FeedImageCellController {
-    private var task: FeedImageDataLoaderTask?
-    private let model: FeedImage
-    private let imageLoader: FeedImageDataLoader
+    private let viewModel: FeedImageViewModel<UIImage>
     
     init(
-        model: FeedImage,
-        imageLoader: FeedImageDataLoader
+        viewModel: FeedImageViewModel<UIImage>
     ) {
-        self.model = model
-        self.imageLoader = imageLoader
+        self.viewModel = viewModel
     }
     
     func view() -> UITableViewCell {
-        let cell = FeedImageCell()
-        
-        cell.locationContainer.isHidden = (self.model.location == nil)
-        cell.locationLabel.text = self.model.location
-        cell.descriptionLabel.text = self.model.description
-        cell.feedImageView.image = nil
-        cell.feedImageRetryButton.isHidden = true
-        cell.feedImageContainer.startAnimating()
-        let loadImage = { [weak self, weak cell] in
-            guard let self = self, let cell = cell else { return }
-            self.task = self.imageLoader.loadImageData(from: self.model.url) { [cell] result in
-                let data = try? result.get()
-                let image = data.map(UIImage.init) ?? nil
-                cell.feedImageView.image = image
-                cell.feedImageRetryButton.isHidden = (image != nil)
-                cell.feedImageContainer.stopAnimating()
-            }
-        }
-        
-        cell.onRetry = loadImage
-        loadImage()
+        let cell = binded(FeedImageCell())
+        viewModel.loadImageData()
         return cell
     }
     
     func preload() {
-        task = imageLoader.loadImageData(from: model.url) { _ in }
+        viewModel.loadImageData()
     }
+    
     func cancelLoad() {
-        task?.cancel()
+        viewModel.cancelImageDataLoad()
+    }
+    
+    private func binded(_ cell: FeedImageCell) -> FeedImageCell {
+        cell.locationContainer.isHidden = !viewModel.hasLocation
+        cell.locationLabel.text = viewModel.location
+        cell.descriptionLabel.text = viewModel.description
+        cell.onRetry = viewModel.loadImageData
+        
+        viewModel.onImageLoad = { [weak cell] image in
+            cell?.feedImageView.image = image
+        }
+        
+        viewModel.onImageLoadingStateChange = { [weak cell] isLoading in
+            cell?.feedImageContainer.isShimmering = isLoading
+        }
+        
+        viewModel.onShouldRetryImageLoadStateChange = { [weak cell] shouldRetry in
+            cell?.feedImageRetryButton.isHidden = !shouldRetry
+        }
+        
+        return cell
     }
 }
