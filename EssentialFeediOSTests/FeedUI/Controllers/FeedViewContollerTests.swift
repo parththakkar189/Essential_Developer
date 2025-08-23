dbc270f3d3b8cd2e5148d2129d5311fefaffc183
Parth Takkhar <parththakkar189@gmail.com>
2025-08-23 18:17:44 -0400

    set `FeedViewController` title

--- EssentialFeediOSTests/FeedUI/Controllers/FeedViewContollerTests.swift
+++ EssentialFeediOSTests/FeedUI/Controllers/FeedViewContollerTests.swift
@@ -12,6 +12,15 @@ import EssentialFeediOS
 
 final class FeedViewContollerTests: XCTestCase {
 
+    
+    func test_feedView_hasTitle() {
+        let (sut, _) = makeSUT()
+        
+        sut.simulateAppearance()
+        
+        XCTAssertEqual(sut.title, "My Feed")
+    }
+    
     func test_loadFeedActions_requestFeedFromLoader() {
         let (sut , loader) = makeSUT()
         
