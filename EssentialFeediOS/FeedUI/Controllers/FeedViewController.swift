6e74d1c16e68aa7a8ba99e1601541bddef0291ce
Parth Takkhar <parththakkar189@gmail.com>
2025-08-23 19:17:35 -0400

    Disptach background feed completion to main thread before updating the UI since UIKit is not thread safe

--- EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
@@ -21,7 +21,15 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     var delegate: FeedViewControllerDelegate?
     var onViewIsAppearing: ((FeedViewController) -> Void)?
     var tableModel = [FeedImageCellController]() {
-        didSet { tableView.reloadData() }
+        didSet {
+            if Thread.isMainThread {
+                tableView.reloadData()
+            } else {
+                DispatchQueue.main.async { [weak self] in
+                    self?.tableView.reloadData()
+                }
+            }
+        }
     }
 
     public override func viewDidLoad() {
@@ -44,6 +52,12 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     func display(_ viewModel: FeedLoadingViewModel) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async { [weak self] in
+                self?.display(viewModel)
+            }
+        }
+
         if viewModel.isLoading {
             refreshControl?.beginRefreshing()
         } else {
--- EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
@@ -316,6 +316,20 @@ final class FeedUIIntegrationTests: XCTestCase {
         XCTAssertEqual(newView.renderedImage, imageData)
     }
     
+    func test_loadFeedCompletion_dispatchesFromBackgroundToMainThread() {
+        let (sut, loader) = makeSUT()
+        sut.simulateAppearance()
+        
+        let exp = expectation(description: "Wait for completion")
+        DispatchQueue.global().async {
+            loader.completeFeedLoading(at: 0)
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+        
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(
