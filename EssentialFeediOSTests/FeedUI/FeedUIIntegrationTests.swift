60f85593716dc85540a88a3036ab3aba7e08b244
Parth Takkhar <parththakkar189@gmail.com>
2025-08-23 18:55:36 -0400

    Add support for Portugese 9pt-BR) localization

--- EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
@@ -13,14 +13,6 @@ import EssentialFeediOS
 final class FeedUIIntegrationTests: XCTestCase {
 
     
-    func test_feedView_hasTitle() {
-        let (sut, _) = makeSUT()
-        
-        sut.simulateAppearance()
-
-        XCTAssertEqual(sut.title, localized("FEED_VIEW_TITLE"))
-    }
-    
     func test_loadFeedActions_requestFeedFromLoader() {
         let (sut , loader) = makeSUT()
         
@@ -357,19 +349,6 @@ final class FeedUIIntegrationTests: XCTestCase {
         return UIImage.make(withColor: .red).pngData()!
     }
 
-    private func localized(
-        _ key: String,
-        file: StaticString = #file,
-        line: UInt = #line
-    ) -> String {
-        let table = "Feed"
-        let bundle = Bundle(for: FeedViewController.self)
-        let value = bundle.localizedString(forKey: key, value: nil, table: table)
-        if value == key {
-            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
-        }
-        return value
-    }
     private func assertThat(
         _ sut: FeedViewController,
         isRendering feed: [FeedImage],
