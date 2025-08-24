44611c14311b9ddafa7f3e75795f41beb3e9a43d
Parth Takkhar <parththakkar189@gmail.com>
2025-08-24 11:43:54 -0400

    Move adapaters and proxy to the separate files.

---
+++ EssentialFeediOS/FeedUI/Composers/WeakRefVirtualProxy.swift
@@ -0,0 +1,34 @@
+//
+//  WeakRefVirtualProxy.swift
+//  EssentialFeed
+//
+//  Created by Parth Thakkar on 2025-08-23.
+//
+
+import Foundation
+import UIKit
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+    
+    func dispatch(_ closure: (T) -> Void) {
+        guard let object = object else { return }
+        closure(object)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
