12bdc50c09dacab1e13c40c12040195d8b5eff52
Parth Takkhar <parththakkar189@gmail.com>
2025-08-24 11:43:54 -0400

    Move adapaters and proxy to the separate files.

---
+++ EssentialFeediOS/FeedUI/Composers/FeedViewAdapter.swift
@@ -0,0 +1,36 @@
+//
+//  FeedViewAdapter.swift
+//  EssentialFeed
+//
+//  Created by Parth Thakkar on 2025-08-23.
+//
+
+import Foundation
+import UIKit
+import EssentialFeed
+
+final class FeedViewAdapter: FeedView {
+    
+    private weak var controller: FeedViewController?
+    private let imageLoader: FeedImageDataLoader
+    
+    init(
+        controller: FeedViewController,
+        imageLoader: FeedImageDataLoader
+    ) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+    
+    func display(_ viewModel: FeedViewModel) {
+        controller?.tableModel = viewModel.feed.map
+            { model in
+                let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
+                let view = FeedImageCellController(delegate: adapter)
+                
+                adapter.presenter = FeedImagePresenter(view: WeakRefVirtualProxy(view), imageTransformer: UIImage.init)
+                
+                return view
+            }
+    }
+}
