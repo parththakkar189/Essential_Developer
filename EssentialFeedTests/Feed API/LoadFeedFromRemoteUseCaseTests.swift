3cb2931baeb2d83e68822a9d1ecf8cb02d117045
Parth Takkhar <parththakkar189@gmail.com>
2025-09-06 15:51:35 -0400

    Extract duplicate `HTTPClientSpy` helper into a shared scope

--- EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -159,35 +159,4 @@ final class LoadFeedFromRemoteUseCaseTests: XCTestCase {
         trackForMemoryLeaks(client)
         return (sut, client)
     }
-    
-    private class HTTPClientSpy: HTTPClient {
-        var requestedURLs: [URL] {
-            return messages.map { $0.url }
-        }
-        private var messages = [(url: URL, completion: (Result) -> Void)]()
-        
-        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
-            messages.append((url, completion))
-            return Task()
-        }
-        
-        func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(.failure(error))
-        }
-        
-        func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
-            let response = HTTPURLResponse(
-                url: requestedURLs[index],
-                statusCode: code,
-                httpVersion: nil,
-                headerFields: nil)!
-            messages[index].completion(.success((data, response)))
-        }
-        
-        // MARK: - Task Wrapper
-        
-        private struct Task: HTTPClientTask {
-            func cancel() { }
-        }
-    }
 }
