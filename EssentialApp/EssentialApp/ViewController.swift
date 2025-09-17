50c2719796a94df51d77aed477c5f7047d020a5a
Parth Takkhar <parththakkar189@gmail.com>
2025-09-17 13:44:29 -0400

    Add empty `EssentialApp` project

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Parth Thakkar on 2025-09-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
